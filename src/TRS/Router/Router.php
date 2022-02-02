98c2e3d3ea89596d926a01ca2c109bd4af291242
Tom <tom@tomroberts.uk>
2022-02-02 11:20:57 +0000

    🐛 namespace / path bug

--- src/TRS/Router/Router.php
+++ src/TRS/Router/Router.php
@@ -1,11 +1,14 @@
 <?php
 
 /**
- * @author      Bram(us) Van Damme <bramus@bram.us>
+ * @author      Tom Roberts <tom@tomroberts.uk> & Bram(us) Van Damme <bramus@bram.us>
  * @copyright   Copyright (c), 2013 Bram(us) Van Damme
  * @license     MIT public license
  */
 namespace TRS\Router;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 /**
  * Class Router.
@@ -46,6 +49,11 @@ class Router
      * @var string Default Controllers Namespace
      */
     private $namespace = '';
+
+    /**
+     * @var RequestInterface The Request
+     */
+    private $request;
 
     /**
      * Store a before middleware route and a handling function to be executed when accessed using one of the specified methods.
@@ -222,20 +230,19 @@ class Router
     public function getRequestMethod()
     {
         // Take the method as found in $_SERVER
-        $method = $_SERVER['REQUEST_METHOD'];
+        $method = $this->request->getMethod();
 
         // If it's a HEAD request override it to being GET and prevent any output, as per HTTP Specification
         // @url http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9.4
-        if ($_SERVER['REQUEST_METHOD'] == 'HEAD') {
+        if ($method == 'HEAD') {
             ob_start();
             $method = 'GET';
         }
 
         // If it's a POST request, check for a method override header
-        elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $headers = $this->getRequestHeaders();
-            if (isset($headers['X-HTTP-Method-Override']) && in_array($headers['X-HTTP-Method-Override'], array('PUT', 'DELETE', 'PATCH'))) {
-                $method = $headers['X-HTTP-Method-Override'];
+        elseif ($method == 'POST') {
+            if($this->request->hasHeader('X-HTTP-Method-Override') && in_array($this->request->getHeader('X-HTTP-Method-Override'), array('PUT', 'DELETE', 'PATCH'))) {
+                $method = $this->request->getHeader('X-HTTP-Method-Override');
             }
         }
 
@@ -480,7 +487,7 @@ class Router
                         if (\is_string($controller)) {
                             $controller = new $controller();
                         }
-                        call_user_func_array(array($controller, $method), $params);
+                        $this->response-> call_user_func_array(array($controller, $method), $params);
                     }
                 }
             }
